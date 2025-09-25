5105b615020eb2966d9f06d8d10b5e05d66717ca
David-23-hue <davidaddo590@gmail.com>
2025-09-25 11:49:45 +0000

    This is my change for Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,23 @@
+// Exercise5_Functions/Program.cs
 using System;
 
 class Program
 {
-    static void Main(string[] args)
+    // Function to add two numbers
+    static double Add(double a, double b)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        return a + b;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter the first number: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        double result = Add(num1, num2);
+        Console.WriteLine($"The sum of {num1} and {num2} is: {result}");
     }
 }

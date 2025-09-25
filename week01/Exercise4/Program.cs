2aaf22a3b9714157fb65464b94b52d426279e266
David-23-hue <davidaddo590@gmail.com>
2025-09-25 11:48:24 +0000

    This is my 4th commit

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,26 @@
+// Exercise4_Lists/Program.cs
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<string> favoriteFoods = new List<string>();
+
+        Console.WriteLine("Enter your 3 favorite foods:");
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write($"Food #{i+1}: ");
+            string food = Console.ReadLine();
+            favoriteFoods.Add(food);
+        }
+
+        Console.WriteLine("\nYour favorite foods are:");
+        foreach (string food in favoriteFoods)
+        {
+            Console.WriteLine(food);
+        }
     }
 }

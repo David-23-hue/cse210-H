f0dc6f06d2b7cb94e8242fbad326b9b7307371ee
David-23-hue <davidaddo590@gmail.com>
2025-09-28 21:53:57 +0000

    This is week 04 Online Ordering Program

---
+++ week04/OnlineOrdering/order.cs
@@ -0,0 +1,46 @@
+// Order.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Order
+{
+    private List<Product> _products;
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+        _products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double CalculateTotal()
+    {
+        double totalProducts = _products.Sum(p => p.GetTotalCost());
+        double shipping = _customer.LivesInUSA() ? 5.0 : 35.0;
+        return totalProducts + shipping;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "Packing Label:\n";
+        foreach (var product in _products)
+        {
+            label += $"- {product.GetName()} (ID: {product.GetProductId()})\n";
+        }
+        return label.Trim();
+    }
+
+    public string GetShippingLabel()
+    {
+        string label = "Shipping Label:\n";
+        label += $"{_customer.GetName()}\n";
+        label += _customer.GetAddress().GetFullAddress();
+        return label.Trim();
+    }
+}
